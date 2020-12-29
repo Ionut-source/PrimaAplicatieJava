15e14dd38846fc6b81a90c9becfeb1bec0eb18dc
cumpa_000 <cumpanasoiu.ui>
2020-12-29 20:06:11 +0200

    Adaugare comentarii pe o singura linie

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
+
+    //Aceasta este metoda main, care porneste aplicatia Java
     public static void main(String[] args) {
+        //Inainte de comentaiu nu se poate pune text
+        //Aici vrem sa afisam pe consola Intelij un mesaj ales de noi
         System.out.println("Invat limbajul Java si despre masina virtuala Java");
       }
     }
